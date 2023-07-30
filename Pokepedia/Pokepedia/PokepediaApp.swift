b1a1f097f285cb0fc3eebd99d3a8dd34f6e7e2c1
Zdenko Čepan <zdenkocepan@MacStudlaZdenko.docsis.vodafone.cz>
2023-07-30 14:18:52 +0200

    create project

---
+++ Pokepedia/Pokepedia/PokepediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokepediaApp.swift
+//  Pokepedia
+//
+//  Created by Zdenko Čepan on 30.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct PokepediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
